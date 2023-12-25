c41ee2dd233b528e4f5ba9435ea19f8eed8cc1e4
Tim <a2453540@gmail.com>
2023-12-25 18:27:32 +0800

    儲存使用者上傳資料和照片

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,8 @@ Route::middleware(['auth', 'roles:admin'])->group(function() {
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
    
     Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+   
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
 
 });
 
