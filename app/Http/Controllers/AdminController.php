e3f1e5f98e12d9ca98e1794f50e954012b769eb8
Tim <a2453540@gmail.com>
2023-12-26 17:13:04 +0800

    更新密碼功能

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -44,8 +45,11 @@ class AdminController extends Controller
         $data->phone = $request->phone;
         $data->address = $request->address;
 
+        // 儲存上傳圖片
         if($request->file('photo')){
             $file = $request->file('photo');
+            // 取代原有圖片
+            @unlink(public_path('upload/admin_images'.$data->photo));
             $filename = date('YmdHi').$file->getClientOriginalName();
             $file->move(public_path('upload/admin_images'), $filename);
             $data['photo'] = $filename;
@@ -59,5 +63,39 @@ class AdminController extends Controller
         );
 
         return redirect()->back()->with($notification);
+    }
+
+    public function AdminChangePassword() {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_change_password', compact('profileData'));
+    }
+
+    public function AdminPasswordUpdate(Request $request) {
+        $request->validate([
+            'old_password'=> 'required',
+            'new_password'=> 'required|confirmed',
+        ]);
+
+        if(!Hash::check($request->old_password, auth::user()->password)){
+            $notification = array(
+                'message' => 'Old Password does not match!',
+                'alert-type'=> 'error'
+            );
+    
+            return back()->with($notification);
+        }
+
+        // 更新新密碼
+        User::whereId(auth::user()->id)->update([
+            'password'=> Hash::make($request->new_password),
+        ]);
+
+        $notification = array(
+            'message' => 'Password Change Successfully',
+            'alert-type'=> 'success'
+        );
+
+        return back()->with($notification);
     }
 }
