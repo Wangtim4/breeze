5d7ffdad1347e95db0a6c421b70666c7ddaf8a2b
Tim <a2453540@gmail.com>
2024-02-03 23:42:37 +0800

    分類的更新功能

--- app/Http/Controllers/Backend/CatrgoryController.php
+++ app/Http/Controllers/Backend/CatrgoryController.php
@@ -51,9 +51,63 @@ class CatrgoryController extends Controller
         // ]);
 
         $notification = array(
-            'message'=> 'Category Inseryed Successfully',
-            'alert'=> 'success',
+            'message'=> 'Category Inserted Successfully',
+            'alert-type'=> 'success',
         );
         return redirect()->route('all.category')->with($notification);
-    }
+    }//END
+
+    public function EditCategory($id) {
+
+        $category = Category::find($id);
+        return view('admin.backend.category.edit_category',compact('category'));
+    }//END
+
+    
+    public function UpdateCategory(Request $request) {
+
+        $category_id = $request->id;
+
+        if($request->file("image")){
+            $manager = new ImageManager(new Driver());
+            $name_gen = hexdec(uniqid()).'.'.$request->file('image')->getClientOriginalExtension();
+            $img = $manager->read($request->file("image"));
+            $img = $img->resize(370,246);
+            
+            $img->toJpeg(80)->save(base_path('public/upload/category/'.$name_gen));
+            $save_url = 'upload/category/'.$name_gen;
+
+             Category::find($category_id)->update([
+            'category_name'=> $request->category_name,
+            'category_slug'=> strtolower(str_replace(' ','-',$request->category_name)),
+            'image' => $save_url,
+            // 需去php.ini改extension=gd 才可使用
+        ]);
+
+        
+        $notification = array(
+            'message'=> 'Category Updated With Image Successfully',
+            'alert-type'=> 'success',
+        );
+        return redirect()->route('all.category')->with($notification);
+        }else{
+            
+            Category::find($category_id)->update([
+                'category_name'=> $request->category_name,
+                'category_slug'=> strtolower(str_replace(' ','-',$request->category_name)),
+            ]);
+
+            $notification = array(
+                'message'=> 'Category Updated Without Image Successfully',
+                'alert-type'=> 'success',
+            );
+            return redirect()->route('all.category')->with($notification);
+        }
+
+
+    
+
+    }//END
+
+
 }
--- resources/views/admin/backend/category/all_category.blade.php
+++ resources/views/admin/backend/category/all_category.blade.php
@@ -41,10 +41,10 @@
                             
                         <tr>
                             <td>{{ $key+1 }}</td>
-                            <td><img src="{{asset($item->image)}}" alt="" style="width: 70px;height: 70px"></td>
+                            <td><img src="{{asset($item->image)}}" alt="" style="width: 70px;height:40px"></td>
                             <td>{{ $item->category_name}}</td>
                             <td>
-                                <a href="" class="btn btn-info px-5">Edit</a>
+                                <a href="{{ route('edit.category',$item->id) }}" class="btn btn-info px-5">Edit</a>
                                 <a href="" class="btn btn-danger px-5">Delete</a>
                             </td>
                         </tr>
