b77dec8a8ed132fb55ee3af605ee034ef2244d08
Tim <a2453540@gmail.com>
2024-02-07 17:35:12 +0800

    新增講師註冊頁面

--- routes/web.php
+++ routes/web.php
@@ -87,6 +87,10 @@ Route::middleware(['auth', 'roles:admin'])->group(function() {
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
 
+Route::get('/become/instructor', [AdminController::class, 'BecomeInstructor'])->name('become.instructor');
+
+Route::post('/instructor/register', [AdminController::class, 'InstructorRegister'])->name('instructor.register');
+
 
 Route::middleware(['auth', 'roles:instructor'])->group(function() {
     
